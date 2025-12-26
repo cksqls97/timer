0354ec44c90371b5b4083994ec94d842f539f6b3
cksqls97 <81712540+cksqls97@users.noreply.github.com>
2025-12-27 03:57:12 +0900

    Update skill.py

--- skill.py
+++ skill.py
@@ -1,18 +1,39 @@
-import keyboard, pyperclip, datetime
-names = [input(f"{i+1}번 이름: ") for i in range(4)]
-e_name = input("5번(13분) 이름: ")
-u = {str(i+1): [names[i], 30, None] for i in range(4)}
-u['5'] = [e_name, 13, None]
-def up():
-    n = datetime.datetime.now()
-    o = [f"{x[0]} {x[2].strftime('%H:%M')}" if x[2] and n < x[2] else x[0] for x in [u[str(i)] for i in range(1, 5)]]
-    ex = f"{u['5'][0]} {u['5'][2].strftime('%H:%M')}" if u['5'][2] and n < u['5'][2] else u['5'][0]
-    res = f"{' '.join(o)} / {ex}"
-    pyperclip.copy(res)
-    print(f"\r[복사됨]: {res}", end="")
-def p(k):
-    u[k][2] = datetime.datetime.now() + datetime.timedelta(minutes=u[k][1])
-    up()
-for i in range(1, 6):
-    keyboard.add_hotkey(f'num {i}', lambda k=str(i): p(k))
-keyboard.wait()
+import keyboard, pyperclip, datetime, sys
+
+def main():
+    try:
+        names = [input(f"{i+1}번 이름: ") for i in range(4)]
+        e_name = input("5번(13분) 이름: ")
+        u = {str(i+1): [names[i], 30, None] for i in range(4)}
+        u['5'] = [e_name, 13, None]
+
+        def up():
+            n = datetime.datetime.now()
+            o = []
+            for i in range(1, 5):
+                name, cool, next_t = u[str(i)]
+                if next_t and n < next_t:
+                    o.append(f"{name} {next_t.strftime('%H:%M')}")
+                else:
+                    o.append(name)
+            
+            e_name_p, e_cool, e_next_t = u['5']
+            ex = f"{e_name_p} {e_next_t.strftime('%H:%M')}" if e_next_t and n < e_next_t else e_name_p
+            res = f"{' '.join(o)} / {ex}"
+            pyperclip.copy(res)
+            print(f"\r[복사됨]: {res}", end="", flush=True)
+
+        def p(k):
+            u[k][2] = datetime.datetime.now() + datetime.timedelta(minutes=u[k][1])
+            up()
+
+        for i in range(1, 6):
+            keyboard.add_hotkey(f'num {i}', lambda k=str(i): p(k))
+
+        print("\n작동 시작 (NUMPAD 1~5) / 종료하려면 Ctrl+C")
+        keyboard.wait()
+    except Exception as e:
+        print(f"에러 발생: {e}")
+
+if __name__ == "__main__":
+    main()
